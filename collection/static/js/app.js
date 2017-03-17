212fcc2bcdb1ce1e5f5e65088d9a7faee5a2155d
ronchris <ponferrada.ronald@gmail.com>
2017-03-17 00:07:13 -0700

    update hp css

--- collection/static/js/app.js
+++ collection/static/js/app.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
+	
 	$(document).on('keyup keypress', 'form input[type="text"]', function(e) {
 		if(e.which == 13) {
 			e.preventDefault();
 			return false;
   		}
 	});
+	
 });
 
