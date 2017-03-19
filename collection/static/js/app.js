a1e01b924ed0c0ad64c04fdb11c37d293484707c
ronchris <ponferrada.ronald@gmail.com>
2017-03-19 00:54:08 -0700

    updated css, destination template

--- collection/static/js/app.js
+++ collection/static/js/app.js
@@ -1,11 +1,78 @@
 $(document).ready(function(){
 	
+	// prevent enter submission 
 	$(document).on('keyup keypress', 'form input[type="text"]', function(e) {
 		if(e.which == 13) {
 			e.preventDefault();
 			return false;
   		}
 	});
+
+	// responsive carousel/slider plugin
+	$('.destination-slides').slick({
+	  dots: true,
+	  infinite: false,
+	  speed: 300,
+	  slidesToShow: 2,
+	  slidesToScroll: 2,
+	  responsive: [
+		{
+		  breakpoint: 1024,
+		  settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2,
+			infinite: true,
+			dots: true
+		  }
+		},
+		{
+		  breakpoint: 600,
+		  settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2
+		  }
+		},
+		{
+		  breakpoint: 480,
+		  settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1
+		  }
+		}
+		// You can unslick at a given breakpoint now by adding:
+		// settings: "unslick"
+		// instead of a settings object
+	  ]
+	});
+	
+	// splice string "," 
+	var myStringActivities = $(".activities-value").text();
+	var myArrayActivities = myStringActivities.split(',');
+	console.log(myArrayActivities);
+	
+	for (var i=0; i<myArrayActivities.length; i++) {
+		var container = $(".activities-container");
+		container.append('<p class="activity--box--destination--activities">' + myArrayActivities[i] + '</p>');
+	}
+	
+	var myStringFeatures = $(".features-value").text();
+	var myArrayFeatures = myStringFeatures.split(',');
+	console.log(myArrayFeatures);
+	
+	for (var i=0; i<myArrayFeatures.length; i++) {
+		var container = $(".features-container");
+		container.append('<p class="activity--box--destination--features">' + myArrayFeatures[i] + '</p>');
+	}
+	
+	var myStringMisc = $(".misc-value").text();
+	var myArrayMisc = myStringMisc.split(',');
+	console.log(myArrayMisc);
+	
+	for (var i=0; i<myArrayMisc.length; i++) {
+		var container = $(".misc-container");
+		container.append('<p class="activity--box--destination--misc">' + myArrayMisc[i] + '</p>');
+	}
 	
 });
 
+	
--- projectx/static/js/app.js
+++ projectx/static/js/app.js
@@ -1,9 +1,49 @@
 $(document).ready(function(){
+	
 	$(document).on('keyup keypress', 'form input[type="text"]', function(e) {
 		if(e.which == 13) {
 			e.preventDefault();
 			return false;
   		}
 	});
+
+	  
+	$('.destination-slides').slick({
+	  dots: true,
+	  infinite: false,
+	  speed: 300,
+	  slidesToShow: 2,
+	  slidesToScroll: 2,
+	  responsive: [
+		{
+		  breakpoint: 1024,
+		  settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2,
+			infinite: true,
+			dots: true
+		  }
+		},
+		{
+		  breakpoint: 600,
+		  settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2
+		  }
+		},
+		{
+		  breakpoint: 480,
+		  settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1
+		  }
+		}
+		// You can unslick at a given breakpoint now by adding:
+		// settings: "unslick"
+		// instead of a settings object
+	  ]
+	});
+	
 });
 
+	
