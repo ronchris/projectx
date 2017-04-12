dded65553ea2ae3360852526dce529dd93127f62
ronchris <ponferrada.ronald@gmail.com>
2017-04-11 20:07:20 -0700

    updated ajax for saves

--- collection/static/js/ajax.js
+++ collection/static/js/ajax.js
@@ -305,7 +305,6 @@ $(document).ready(function(){
 				success: function (e) {
 					console.log(e);
 					$('.saves').css("background-color", "#a2db68");
-					$('.saves').text("SAVED");
 				}
 			});
 		}
